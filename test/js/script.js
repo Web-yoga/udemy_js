1591cee2cef0f8c571b42e79e99f928e58b11333
Alexander Tsvetkov <hyehaeng@gmail.com>
2020-12-05 14:32:30 +0300

    first commit

---
+++ test/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+/* Урок 8 */
+//Вывод переменной в строке
+let x = "переменная";
+let res = `${x} вставляется в строку методом интерполяции`;
+console.log(res);
+
+/* Урок 8 */
+alert("Hello");
+let number = 5;
+const leftBorderWidth = 1;
+console.log(number);
