f25ed906183872863636b334d0504cae087ea361
Alexander Tsvetkov <hyehaeng@gmail.com>
2020-12-06 11:59:39 +0300

    первое упражнение

--- test/js/script.js
+++ test/js/script.js
@@ -5,8 +5,19 @@ let x = "переменная";
 let res = `${x} вставляется в строку методом интерполяции`;
 console.log(res);
 
-/* Урок 8 */
-alert("Hello");
-let number = 5;
-const leftBorderWidth = 1;
-console.log(number);
+/* Урок 12 */
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const   a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", ""),
+        c = prompt("Один из последних просмотренных фильмов?", ""),
+        d = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
